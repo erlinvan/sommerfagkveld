2315133fa3f35914c598033e3fbdcaec62ea2e06
Erling Van De Weijer <44138705+erlinvan@users.noreply.github.com>
2025-07-15 10:34:57 +0200

    Remove unused import.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
-import { JenteMedGlass } from "./JenteMedGlass";
 import { Glass } from "./Glass";
 
 export function Navbar() {
